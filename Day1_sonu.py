ed3988fa7dd4e5c0e95bb629e572bbc03ca0a587
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-09-14 21:35:05 +0530

    assignment submitted of day 1

---
+++ Day1_sonu.py
@@ -0,0 +1,16 @@
+#ques 1
+a="sonu"
+b="sonu"
+print(a is b)
+c=5.6667
+d=5.6667
+print(c is d)
+#ques 2
+print(1>3>4)
+#Question no. 3
+str='180'
+print("int('180') with base 2 = ", int(str,2))
+s=str
+print("value of str function = ",s)
+float_string=float("-12")
+print("value of float function = ",float_string)
